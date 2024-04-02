b04b8f1ce1d51406f167d579e4cd1c78d93ce635
Davyd McColl <davydm@gmail.com>
2024-04-02 15:01:16 +0200

    :tada: initial commit, demonstrates the issue

---
+++ src/streamify.ts
@@ -0,0 +1,67 @@
+import { Transform } from "stream";
+import * as vinyl from "vinyl";
+const through = require("through2");
+const PluginError = require("plugin-error");
+
+type AsyncTVoid<T> = (arg: T) => Promise<void>;
+type OptionsFactory<T> = (file: vinyl.BufferFile) => T | Promise<T>;
+
+interface PluginError
+  extends Error {
+  new(pluginName: string, err: string | Error): void;
+
+  verbosity?: string;
+}
+
+export function streamify_original<T>(
+  fn: AsyncTVoid<T>,
+  optionsFactory: OptionsFactory<T>,
+  pluginName: string,
+  operation: string
+): Transform {
+  return through.obj(async function (
+    this: Transform,
+    file: vinyl.BufferFile,
+    enc: string,
+    cb: (err: PluginError | null, file: vinyl.BufferFile) => void
+  ) {
+    try {
+      const options = await optionsFactory(file);
+      await fn(options);
+      cb(null, file);
+    } catch (e: unknown) {
+      const pluginError = new PluginError(
+        pluginName,
+        `${ operation } failed: ${ (e as Error).message || e }`
+      );
+      this.emit("error", pluginError);
+      cb(pluginError, file);
+    }
+  })
+}
+
+export function streamify_perdocs<T>(
+  fn: AsyncTVoid<T>,
+  optionsFactory: OptionsFactory<T>,
+  pluginName: string,
+  operation: string
+): Transform {
+  // Monkey patch Transform or create your own subclass,
+  // implementing `_transform()` and optionally `_flush()`
+  var transformStream = new Transform({objectMode: true});
+  /**
+   * @param {Buffer|string} file
+   * @param {string=} encoding - ignored if file contains a Buffer
+   * @param {function(Error, object)} callback - Call this function (optionally with an
+   *          error argument and data) when you are done processing the supplied chunk.
+   */
+  transformStream._transform = async function(file, encoding, callback) {
+    const
+      error = null,
+      options = await optionsFactory(file),
+      output = await fn(options);
+    callback(error, output);
+  };
+
+  return transformStream;
+}
---
+++ tests/streamify.spec.ts
@@ -0,0 +1,93 @@
+import "expect-even-more-jest";
+import * as through from "through2";
+
+const { Sandbox } = require("filesystem-sandbox");
+import * as vinyl from "vinyl";
+import * as gulp from "gulp";
+import { streamify_original, streamify_perdocs } from "../src/streamify";
+
+describe(`streamify-async-function`, () => {
+  interface FooOpts {
+    target: string;
+    flag?: boolean;
+  }
+
+  const captured = {} as any;
+
+  async function foo(opts: FooOpts): Promise<void> {
+    captured.opts = opts;
+  }
+
+  describe(`original function`, () => {
+    it(`should provide a new function taking the same arguments, which can be streamed`, async () => {
+      // Arrange
+      const sandbox = await Sandbox.create();
+      const txtFile = await sandbox.writeFile("foo.txt", "moo-cow");
+      // Act
+      await new Promise<void>(resolve => {
+        gulp.src(`${ sandbox.path }/**/*.txt`)
+          .pipe(streamify_original(
+              foo,
+              (f: vinyl.BufferFile) => {
+                return {
+                  target: f.path,
+                  flag: true
+                }
+              },
+              "test plugin",
+              "foo"
+            )
+          ).pipe(
+          through.obj(function () {
+              resolve();
+            }
+          )
+        );
+      });
+      // Assert
+      expect(captured.opts.target)
+        .toEqual(txtFile)
+    });
+  });
+
+  describe(`modified example function`, () => {
+    it(`should provide a new function taking the same arguments, which can be streamed`, async () => {
+      // Arrange
+      const sandbox = await Sandbox.create();
+      const txtFile = await sandbox.writeFile("foo.txt", "moo-cow");
+      // Act
+      await new Promise<void>(resolve => {
+        gulp.src(`${ sandbox.path }/**/*.txt`)
+          .pipe(streamify_perdocs(
+              foo,
+              (f: vinyl.BufferFile) => {
+                return {
+                  target: f.path,
+                  flag: true
+                }
+              },
+              "test plugin",
+              "foo"
+            )
+          ).pipe(
+          through.obj(function () {
+              resolve();
+            }
+          )
+        );
+      });
+      // Assert
+      expect(captured.opts.target)
+        .toEqual(txtFile)
+    });
+  });
+
+  beforeEach(() => {
+    for (const k of Object.keys(captured)) {
+      delete captured[k];
+    }
+  });
+  afterEach(async () => {
+    await Sandbox.destroyAll();
+  });
+});
