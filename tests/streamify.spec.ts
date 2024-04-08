c99d30623284bed6b15cfb99f18b275c807599b3
Davyd McColl <davydm@gmail.com>
2024-04-08 09:13:18 +0200

    :ok_hand: works if we run within the sandbox folder (gulp stream issue)
    (apparently resolved in https://github.com/gulpjs/glob-stream/pull/131)

--- tests/streamify.spec.ts
+++ tests/streamify.spec.ts
@@ -6,7 +6,7 @@ const sink = require("lead");
 const { Sandbox } = require("filesystem-sandbox");
 import * as vinyl from "vinyl";
 import * as gulp from "gulp";
-import { streamify_original, streamify_perdocs, streamify_perdocs_sync } from "../src/streamify";
+import { streamify_original } from "../src/streamify";
 
 describe(`streamify-async-function`, () => {
   interface FooOpts {
@@ -24,68 +24,24 @@ describe(`streamify-async-function`, () => {
     captured.opts = opts;
   }
 
-  describe(`lead`, () => {
-    it(`should work as per the readme example`, async () => {
-      // Arrange
-      const { Readable, Transform } = require("streamx");
-      const sink = require("lead");
-      let calls = 0;
-      // Act
-      const maybeThrough = new Transform({
-        transform(chunk: any, cb: any) {
-          calls++;
-          cb(null, chunk);
-        }
-      });
-
-      // Assert
-      // this is the example as at https://github.com/gulpjs/lead
-      Readable.from(['hello', 'world'])
-        // Sink it to behave like a Writeable
-        .pipe(sink(maybeThrough));
-      await sleep(100); // stream should be done by then
-      expect(calls)
-        .toEqual(2);
-
-      // now, what if we do the same thing, but with gulp.src
-      // as the source?
-      const
-        sandbox = await Sandbox.create();
-
-      await sandbox.writeFile("first.txt", "first");
-      await sandbox.writeFile("second.txt", "second");
-      await sandbox.writeFile("third.txt", "third");
-      gulp.src(`${sandbox.path}/*`)
-        .pipe(sink(maybeThrough));
-      await new Promise(resolve => setTimeout(resolve, 100));
-      // this _will_ fail, because the transform _wasn't_ called.
-      expect(calls)
-        .toEqual(5);
-    });
-  });
-
-  async function sleep(ms: number): Promise<void> {
-    await new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  describe(`discovery`, () => {
+  describe(`discovery: example plugin code`, () => {
     it(`should complete`, async () => {
       // Arrange
       const sandbox = await Sandbox.create();
       await sandbox.writeFile("foo.txt", "moo-cow");
       // Act
-      await new Promise<void>(resolve => {
-
-        gulp.src(`${ sandbox.path }/*`)
-          .pipe(
-            through.obj(function (chunk, enc, cb) {
-                console.log("-- going through the pipe ---");
-                cb();
-                resolve();
-              }
-            )
-          );
-
+      await sandbox.run(async () => {
+        await new Promise<void>(resolve => {
+          gulp.src(`*`)
+            .pipe(
+              through.obj(function (chunk, enc, cb) {
+                  console.log("-- going through the pipe ---");
+                  cb();
+                  resolve();
+                }
+              )
+            );
+        });
       });
     });
   });
@@ -96,93 +52,31 @@ describe(`streamify-async-function`, () => {
       const sandbox = await Sandbox.create();
       const txtFile = await sandbox.writeFile("foo.txt", "moo-cow");
       // Act
-      await new Promise<void>(resolve => {
-        gulp.src(`${ sandbox.path }/**/*.txt`)
-          .pipe(
-            sink(
-              streamify_original(
-                foo,
-                (f: vinyl.BufferFile) => {
-                  return {
-                    target: f.path,
-                    flag: true
-                  }
-                },
-                "test plugin",
-                "foo"
+      await sandbox.run(async () => {
+        await new Promise<void>(resolve => {
+          gulp.src(`**/*.txt`)
+            .pipe(
+              sink(
+                streamify_original(
+                  foo,
+                  (f: vinyl.BufferFile) => {
+                    return {
+                      target: f.path,
+                      flag: true
+                    }
+                  },
+                  "test plugin",
+                  "foo"
+                )
               )
             )
-          )
-          .pipe(
-            through.obj(function () {
-                resolve();
-              }
-            )
-          ).pipe(gulp.dest(sandbox.path));
-      });
-      // Assert
-      expect(captured.opts.target)
-        .toEqual(txtFile)
-    });
-  });
-
-  describe(`modified example function`, () => {
-    it(`should provide a new function taking the same arguments, which can be streamed`, async () => {
-      // Arrange
-      const sandbox = await Sandbox.create();
-      const txtFile = await sandbox.writeFile("foo.txt", "moo-cow");
-      // Act
-      await new Promise<void>(resolve => {
-        gulp.src(`${ sandbox.path }/**/*.txt`)
-          .pipe(streamify_perdocs(
-              foo,
-              (f: vinyl.BufferFile) => {
-                return {
-                  target: f.path,
-                  flag: true
+            .pipe(
+              through.obj(function () {
+                  resolve();
                 }
-              },
-              "test plugin",
-              "foo"
-            )
-          ).pipe(
-          through.obj(function () {
-              resolve();
-            }
-          )
-        );
-      });
-      // Assert
-      expect(captured.opts.target)
-        .toEqual(txtFile)
-    });
-  });
-
-  describe(`modified example function (sync)`, () => {
-    it(`should provide a new function taking the same arguments, which can be streamed`, async () => {
-      // Arrange
-      const sandbox = await Sandbox.create();
-      const txtFile = await sandbox.writeFile("foo.txt", "moo-cow");
-      // Act
-      await new Promise<void>(resolve => {
-        gulp.src(`${ sandbox.path }/**/*.txt`)
-          .pipe(streamify_perdocs_sync(
-              foo_sync,
-              (f: vinyl.BufferFile) => {
-                return {
-                  target: f.path,
-                  flag: true
-                }
-              },
-              "test plugin",
-              "foo"
-            )
-          ).pipe(
-          through.obj(function () {
-              resolve();
-            }
-          )
-        );
+              )
+            ).pipe(gulp.dest("_build"));
+        });
       });
       // Assert
       expect(captured.opts.target)
